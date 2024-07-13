fd4529356b806a2112c1f4c91b9ed3c5adba62ee
shubham <thekingshubh07@gmail.com>
2024-07-13 13:54:00 +0530

    Updated comments

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from langchain_helper import get_few_shot_db_chain
 
-st.title("RetailQueryGen AI : Fashion store Database Q&A 👕")
+st.title("RetailQueryGen AI : Fashion store, Q&A with Database 👕🏬🛒")
 
 question = st.text_input("Question: ")
 
